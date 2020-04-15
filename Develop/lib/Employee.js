f1e350cf7a92909c75a4f4905f388785a0742480
akandiew <andrewkandiew@Andrews-MBP.home>
2020-04-15 19:40:46 -0400

    resolve Employee testing

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,32 @@
 // TODO: Write code to define and export the Employee class
+class   Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        
+    }
+    getName()   {
+        return this.name;
+
+    }
+    getID() {
+        return this.id;
+
+    }
+    getEmail()  {
+        return this.email;
+
+    }
+    
+    
+
+    printInfo() {
+        console.log(`Employee name is: ${this.name}`);
+        console.log(`Employee ID is: ${this.id}`);
+        console.log(`Employee e-mail is: ${this.email}`);
+    };
+
+};
+
+module.exports = Employee;
