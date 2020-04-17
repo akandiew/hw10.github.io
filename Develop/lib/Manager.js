f569634467c531c74adcb50a1951315326b9e81a
akandiew <andrewkandiew@Andrews-MBP.home>
2020-04-16 22:28:29 -0400

    2 suites: 13 tests

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -2,13 +2,13 @@
 const Employee  = require("./employee");
 
 class Manager   extends Employee {
-  constructor(name, id, email, officeNumber) {
+  constructor(name, id, email, office) {
     super(name, id, email)  
-    this.officeNumber = officeNumber;
+    this.office = office;
   }
 
-  getofficeNumber() {
-      return this.officeNumber
+  getOffice() {
+      return this.office
   } 
   
   getRole() {
